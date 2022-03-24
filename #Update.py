3eb44efc953c8253957837844a2e67cf140d322d
killerbox04 <killerbox04@gmail.com>
2022-03-24 11:11:56 -0400

    Update module

--- #Update.py
+++ #Update.py
@@ -2,39 +2,31 @@
 import json
 import time
 
-global X1
-global X2
-global X4
-global X8
+global clk 
 
 X1 = 0
 X2 = 0
 X4 = 0
 X8 = 0
+clk = 0
 
 
-testRange = range(1,17)
 testlist = 1, 2, 4, 8
 
 
-def update(clk):
-    global X1
-    global X2
-    global X4
-    global X8
+def update(Uclk):
 
-    for val in testlist:
-        if clk % val == 0:
-            if val == 1:
-                X1 = flip(X1)
-            if val == 2:
-                X2 = flip(X2)
-            if val == 4:
-                X4 = flip(X4)
-            if val == 8:
-                X8 = flip(X8)    
-        
-        return(X1, X2, X4, X8)
+    if Uclk % 1 == 0:
+        flip(X1)
+        if Uclk
+    if Uclk % 2 == 0:
+        flip(X2)
+    if Uclk % 4 == 0:
+        flip(X4)
+    if Uclk % 8 == 0:
+        flip(X8)
+
+    return(X8, X4, X2, X1)
 
 
 def flip(val):
@@ -45,9 +37,4 @@ def flip(val):
         newVal = 0
 
     return newVal
-
-
-for val in testRange:
-    pval = update(val)
-    print(pval)
-    time.sleep(1)
+    
